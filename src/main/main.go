61d0d986f19782aafb68968901ff01b70daeddb9
UTAGAWA Kiki <utagawakiki@gmail.com>
2017-12-16 23:54:19 +0900

    gofmt

--- src/main/main.go
+++ src/main/main.go
@@ -1,23 +1,23 @@
 package main
 
 import (
-    "os"
-    "github.com/bluele/slack"
+	"github.com/bluele/slack"
+	"os"
 )
 
-func main () {
-    slackToken := os.Getenv("MIR_SLACK_TOKEN")
-    channelName := os.Getenv("MIR_CHANNEL_NAME")
-    api := slack.New(slackToken)
-    channel, err := api.FindChannelByName(channelName)
-    if err != nil {
-        panic(err)
-    }
-    options := &slack.ChatPostMessageOpt{
-        Username: "赤城みりあ",
-        IconUrl: "https://gyazo.com/04a9d82be786486aa2c44463e9e5b60d.png"}
-    err = api.ChatPostMessage(channel.Id, "みりあもやるー！", options)
-    if err != nil {
-        panic(err)
-    }
+func main() {
+	slackToken := os.Getenv("MIR_SLACK_TOKEN")
+	channelName := os.Getenv("MIR_CHANNEL_NAME")
+	api := slack.New(slackToken)
+	channel, err := api.FindChannelByName(channelName)
+	if err != nil {
+		panic(err)
+	}
+	options := &slack.ChatPostMessageOpt{
+		Username: "赤城みりあ",
+		IconUrl:  "https://gyazo.com/04a9d82be786486aa2c44463e9e5b60d.png"}
+	err = api.ChatPostMessage(channel.Id, "みりあもやるー！", options)
+	if err != nil {
+		panic(err)
+	}
 }
